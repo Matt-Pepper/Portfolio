7b2ee98f95ba5bd8f4298c3af1687294fd70149e
Matt-Pepper <matthew.g.pepper@gmail.com>
2023-01-12 14:07:17 +1100

    Added projects

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,13 @@
 function scrollToSection(section) {
-	const anchor = document.getElementById(section).offsetTop;
-	window.scrollTo({
-		top: anchor - 60,
-		behavior: "smooth",
-	});
+    const anchor = document.getElementById(section).offsetTop;
+    window.scrollTo({
+        top: anchor - 60,
+        behavior: "smooth",
+    });
 
-	if (!query.matches) {
-		toggleNavMenu();
-	}
+    if (!query.matches) {
+        toggleNavMenu();
+    }
 }
 
 const query = this.window.matchMedia("(min-width: 640px)");
@@ -17,74 +17,76 @@ const projectsTabs = document.querySelectorAll(".projects__container");
 const projectTabBar = document.querySelectorAll(".projects__tab");
 
 function toggleNavMenu() {
-	hamburger.classList.toggle("nav__hamburger--hidden");
-	hamburger.classList.toggle("nav__hamburger");
+    hamburger.classList.toggle("nav__hamburger--hidden");
+    hamburger.classList.toggle("nav__hamburger");
 
-	navMenu.classList.toggle("nav__menu--media");
-	navMenu.classList.toggle("nav__menu--active");
+    navMenu.classList.toggle("nav__menu--media");
+    navMenu.classList.toggle("nav__menu--active");
 }
 
 window.addEventListener(
-	"resize",
-	function () {
-		if (query.matches) {
-			closeNav();
-		}
-	},
-	true
+    "resize",
+    function () {
+        if (query.matches) {
+            closeNav();
+        }
+    },
+    true
 );
 
 function closeNav() {
-	hamburger.classList.add("nav__hamburger");
-	navMenu.classList.add("nav__menu--media");
+    hamburger.classList.add("nav__hamburger");
+    navMenu.classList.add("nav__menu--media");
 
-	navMenu.classList.remove("nav__menu--active");
-	hamburger.classList.remove("nav__hamburger--hidden");
+    navMenu.classList.remove("nav__menu--active");
+    hamburger.classList.remove("nav__hamburger--hidden");
 }
 
 function selectProjectTab(projectNum) {
-	projectsTabs.forEach((item) => {
-		item.classList.remove("projects__container--active");
+    projectsTabs.forEach((item) => {
+        item.classList.remove("projects__container--active");
 
-		document
-			.getElementById(`project${projectNum}`)
-			.classList.add("projects__container--active");
-	});
+        document
+            .getElementById(`project${projectNum}`)
+            .classList.add("projects__container--active");
+    });
 
-	projectTabBar.forEach((item) => {
-		item.classList.remove("projects__tab--active");
+    projectTabBar.forEach((item) => {
+        item.classList.remove("projects__tab--active");
 
-		document.getElementById(`projectTab${projectNum}`).classList.add("projects__tab--active");
-	});
+        document.getElementById(`projectTab${projectNum}`).classList.add("projects__tab--active");
+    });
 }
 
 function copyToClipboard() {
-	const email = `matthew.g.pepper@gmail.com`;
-	const tooltip = document.querySelector(".contact__tooltip");
+    const email = `matthew.g.pepper@gmail.com`;
+    const tooltip = document.querySelector(".contact__tooltip");
 
-	navigator.clipboard.writeText(email).then(
-		async () => {
-			//success
-			tooltip.classList.add("contact__tooltip--visible");
-			await new Promise((r) => setTimeout(r, 2500));
-			tooltip.classList.remove("contact__tooltip--visible");
-		},
-		() => {
-			// failed
-			console.log("Failed to copy to clipboard.");
-		}
-	);
+    navigator.clipboard.writeText(email).then(
+        async () => {
+            //success
+            tooltip.classList.add("contact__tooltip--visible");
+            await new Promise((r) => setTimeout(r, 2500));
+            tooltip.classList.remove("contact__tooltip--visible");
+        },
+        () => {
+            // failed
+            console.log("Failed to copy to clipboard.");
+        }
+    );
+
+    window.location.href = `mailto:${email}`;
 }
 
 let scrolling = false;
 
 document.addEventListener("scroll", (e) => {
-	if (!scrolling) {
-		window.requestAnimationFrame(() => {
-			closeNav();
-			scrolling = false;
-		});
+    if (!scrolling) {
+        window.requestAnimationFrame(() => {
+            closeNav();
+            scrolling = false;
+        });
 
-		scrolling = true;
-	}
+        scrolling = true;
+    }
 });
